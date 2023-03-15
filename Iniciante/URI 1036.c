26c2987a3eea05e314e87f1ce078887b0ae16048
Vinicius Choma <109703798+vinichoma@users.noreply.github.com>
2023-03-15 11:52:44 -0300

    Update URI 1036.c

--- Iniciante/URI 1036.c
+++ Iniciante/URI 1036.c
@@ -1,4 +1,16 @@
-// https://www.beecrowd.com.br/judge/pt/problems/view/1036
+/*
+Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes, 
+mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.
+
+Entrada
+Leia três valores de ponto flutuante (double) A, B e C.
+
+Saída
+Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular". Caso contrário,
+imprima o resultado das raízes com 5 dígitos após o ponto, com uma mensagem correspondente conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.
+*/
+
+
 
 #include <stdio.h>
 #include <math.h>
