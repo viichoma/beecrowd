5df583cfd16391f1f5a2995f870b729823f31405
Vinicius Choma <109703798+vinichoma@users.noreply.github.com>
2023-03-15 12:09:06 -0300

    Update URI 1101.py

--- Iniciante/URI 1101.py
+++ Iniciante/URI 1101.py
@@ -1,4 +1,12 @@
 # https://www.beecrowd.com.br/judge/pt/problems/view/1101
+# Leia um conjunto não determinado de pares de valores M e N (parar quando algum dos valores for menor ou igual a zero). 
+# Para cada par lido, mostre a sequência do menor até o maior e a soma dos inteiros consecutivos entre eles (incluindo o N e M).
+
+# Entrada
+# O arquivo de entrada contém um número não determinado de valores M e N. A última linha de entrada vai conter um número nulo ou negativo.
+
+# Saída
+# Para cada dupla de valores, imprima a sequência do menor até o maior e a soma deles, conforme exemplo abaixo.
 
 maior=0
 menor= 0
