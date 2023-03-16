6ee691f8a1d726b70eefc1c0ecd0d245fb3c4dd9
Vinicius Choma <109703798+vinichoma@users.noreply.github.com>
2023-03-16 01:34:27 -0300

    Add files via upload

---
+++ Iniciante/URI 3224.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string a, b; cin >> a >> b;
+    if (a.length() < b.length())
+    printf("no\n");
+    else
+    printf("go\n");
+
+}
---
+++ Iniciante/URI 3224.py
@@ -0,0 +1,6 @@
+a = str(input())
+b = str(input())
+if len(a) < len(b):
+    print("no")
+else:
+    print("go")
